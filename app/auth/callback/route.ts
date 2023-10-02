832dffd926eda5409e250241dd35516c32dc4df5
Irene D. Martinez <imartinez921@gmail.com>
2023-10-02 12:41:59 -0400

    Callback checks state CSRF token in auth response

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,21 +1,26 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
-
-import type { NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import type { Database } from "@/lib/database.types";
 
 export const dynamic = "force-dynamic";
 
-export async function GET(request: NextRequest) {
+export async function GET(request: NextRequest, response: NextResponse) {
+	// Extract state string from cookies
+	const stateCookie = cookies().get("state");
+	const stateCheck = stateCookie ? stateCookie.value : undefined;
+
+	// Extract state CSRF token and authorization code
 	const requestUrl = new URL(request.url);
 	const code = requestUrl.searchParams.get("code");
+	const paramsState = requestUrl.searchParams.get("state");
 
-	if (code) {
-		const supabase = createRouteHandlerClient<Database>({ cookies });
-		await supabase.auth.exchangeCodeForSession(code);
+	if (code && paramsState === stateCheck) {
+		console.log("CSRF token: PASS");
+	} else {
+		console.log("CSRF token: FAIL");
 	}
 
 	// URL to redirect to after sign in process completes
-	return NextResponse.redirect(requestUrl.origin);
+	return NextResponse.redirect(`${process.env.SANDBOX_AFTER_LOGIN_URL}`);
 }
