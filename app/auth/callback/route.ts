db6ec9072d174f5234d43d7b9ac8401c70927c2a
Irene D. Martinez <imartinez921@gmail.com>
2023-10-02 16:27:34 -0400

    TODO: Fix request to ObtainToken

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,5 +1,5 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextResponse, NextRequest } from "next/server";
 import type { Database } from "@/lib/database.types";
 
@@ -14,23 +14,55 @@ export async function GET(request: NextRequest, response: NextResponse) {
 	const requestUrl = new URL(request.url);
 	const code = requestUrl.searchParams.get("code");
 	const paramsState = requestUrl.searchParams.get("state");
+	const error = requestUrl.searchParams.get("error");
+	const errorDescription = requestUrl.searchParams.get("error_description");
 
-	if (paramsState === stateCheck) {
-		console.log("CSRF token: PASS");
-	} else {
+	//developer.squareup.com/docs/oauth-api/receive-and-manage-tokens#handle-the-authorization-response
+	if (error) {
+		return NextResponse.json({
+			errorMessage: errorDescription,
+		});
+	}
+
+	if (paramsState !== stateCheck) {
 		return NextResponse.json({
 			errorMessage: "CSRF token check failed. Please try again.",
 		});
 	}
 
+	const clientId = process.env.SANDBOX_APP_ID;
+	const clientSecret = process.env.SANDBOX_APP_SECRET;
+	const redirectUrl = process.env.SANDBOX_APP_REDIRECT_URL;
+
 	if (code) {
-		console.log("GOT AN AUTH CODE!");
-	} else {
-		return NextResponse.json({
-			errorMessage: "Authorization failed. Please try again.",
-		});
+		// Create request to exchange authorization code for access token
+		// (https://developer.squareup.com/docs/oauth-api/what-it-does#authorization-code-flow)
+		const responseBody = {
+			method: "POST",
+			headers: {
+				"Square-Version": "2023-09-25",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				client_id: clientId,
+				client_secret: clientSecret,
+				code: code,
+				grant_type: "authorization_code",
+			}),
+		};
+			console.log("RESPONSE BODY", responseBody);
+		  const response = await fetch(
+				"https://connect.squareup.com/oauth2/token", responseBody
+				
+			);
+			console.log(response)
+			const data = await response.json();
+			return NextResponse.json({ data });
+
 	}
 
-	// URL to redirect to after sign in process completes
-	return NextResponse.redirect(`${process.env.SANDBOX_AFTER_LOGIN_URL}`);
+	return NextResponse.json({
+		errorMessage: "Authorization failed. Please try again.",
+	});
 }
+
