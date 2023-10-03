229ffce651ffbdd7f6d885266a41fffcd2556f7c
Irene D. Martinez <imartinez921@gmail.com>
2023-10-03 16:18:08 -0400

    Remove logins from landing page

--- app/page.tsx
+++ app/page.tsx
@@ -72,22 +72,7 @@ export default async function Index() {
 					<h1 className="sr-only">
 						Supabase and Next.js Starter Template
 					</h1>
-					<br />
-					<br />
-					<br />
-					{/* TODO: Add authorization link to bring user to Square to log in */}
-					<Link
-						href="/auth/sign-in"
-						className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-							<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-								Get started by logging into into your Square seller account.
-							</p>
-						</div>
-					</Link>
+					
 				</div>
 
 				<div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent" />
