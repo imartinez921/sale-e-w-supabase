e426366b794e05dfac755a55db020f92a81e5503
Noah Medoff <noahmedoff@gmail.com>
2023-10-03 14:07:32 -0700

    put customers into customer api

---
+++ app/utils/customer-data-array.ts
@@ -0,0 +1,287 @@
+export const customersCreationData = [
+    {
+        givenName: 'Amelia',
+        familyName: 'Earhart',
+        emailAddress: 'Amelia.Earhart@example.com',
+        address: {
+            addressLine1: '500 Electric Ave',
+            addressLine2: 'Suite 600',
+            locality: 'New York',
+            administrativeDistrictLevel1: 'NY',
+            postalCode: '10003',
+            country: 'US'
+        },
+        phoneNumber: '+1-212-555-4240',
+        note: 'a customer'
+    },
+    {
+        givenName: 'Albert',
+        familyName: 'Einstein',
+        emailAddress: 'Albert.Einstein@example.com',
+        address: {
+            addressLine1: '400 Physics Rd',
+            addressLine2: 'Apt 4',
+            locality: 'Princeton',
+            administrativeDistrictLevel1: 'NJ',
+            postalCode: '08540',
+            country: 'US'
+        },
+        phoneNumber: '+1-609-555-1234',
+        note: 'a customer'
+    },
+    {
+        givenName: 'Marie',
+        familyName: 'Curie',
+        emailAddress: 'Marie.Curie@example.com',
+        address: {
+            addressLine1: '300 Radium St',
+            addressLine2: 'Suite 300',
+            locality: 'Paris',
+            administrativeDistrictLevel1: 'IDF',
+            postalCode: '75005',
+            country: 'FR'
+        },
+        phoneNumber: '+1-212-555-1234',
+        note: 'a customer'
+    },
+    {
+        givenName: 'Isaac',
+        familyName: 'Newton',
+        emailAddress: 'Isaac.Newton@example.com',
+        address: {
+            addressLine1: '200 Gravity Ln',
+            addressLine2: 'Floor 2',
+            locality: 'London',
+            administrativeDistrictLevel1: 'ENG',
+            postalCode: 'SW1A 1AA',
+            country: 'GB'
+        },
+        phoneNumber: '+1-312-678-9012',
+        note: 'a customer'
+    },
+    // {
+    //     givenName: 'Nikola',
+    //     familyName: 'Tesla',
+    //     emailAddress: 'Nikola.Tesla@example.com',
+    //     address: {
+    //         addressLine1: '700 Energy St',
+    //         addressLine2: 'Apt 7',
+    //         locality: 'New York',
+    //         administrativeDistrictLevel1: 'NY',
+    //         postalCode: '10004',
+    //         country: 'US'
+    //     },
+    //     phoneNumber: '+1-212-555-5678',
+    //     note: 'a customer'
+    // },
+    // // {
+    //     givenName: 'Galileo',
+    //     familyName: 'Galilei',
+    //     emailAddress: 'Galileo.Galilei@example.com',
+    //     address: {
+    //         addressLine1: '1600 Telescope Ave',
+    //         addressLine2: 'Suite 16',
+    //         locality: 'Pisa',
+    //         administrativeDistrictLevel1: 'TUS',
+    //         postalCode: '56126',
+    //         country: 'IT'
+    //     },
+    //     phoneNumber: '+39-050-555-7891',
+    //     note: 'a customer'
+    // },
+    {
+        givenName: 'Ada',
+        familyName: 'Lovelace',
+        emailAddress: 'Ada.Lovelace@example.com',
+        address: {
+            addressLine1: '100 Algorithm Rd',
+            addressLine2: 'Apt 10',
+            locality: 'London',
+            administrativeDistrictLevel1: 'ENG',
+            postalCode: 'SW1A 2AA',
+            country: 'GB'
+        },
+        phoneNumber: '+1-415-789-3456',
+        note: 'a customer'
+    },
+    // {
+    //     givenName: 'Thomas',
+    //     familyName: 'Edison',
+    //     emailAddress: 'Thomas.Edison@example.com',
+    //     address: {
+    //         addressLine1: '800 Invention Blvd',
+    //         addressLine2: 'Suite 8',
+    //         locality: 'Menlo Park',
+    //         administrativeDistrictLevel1: 'NJ',
+    //         postalCode: '08820',
+    //         country: 'US'
+    //     },
+    //     phoneNumber: '+1-732-555-1234',
+    //     note: 'a customer'
+    // },
+    // {
+    //     givenName: 'Michael',
+    //     familyName: 'Faraday',
+    //     emailAddress: 'Michael.Faraday@example.com',
+    //     address: {
+    //         addressLine1: '900 Magnet St',
+    //         addressLine2: 'Apt 9',
+    //         locality: 'London',
+    //         administrativeDistrictLevel1: 'ENG',
+    //         postalCode: 'SW1A 3AA',
+    //         country: 'GB'
+    //     },
+    //     phoneNumber: '+44-20-555-3456',
+    //     note: 'a customer'
+    // },
+    // {
+    //     givenName: 'James',
+    //     familyName: 'Maxwell',
+    //     emailAddress: 'James.Maxwell@example.com',
+    //     address: {
+    //         addressLine1: '1100 Lightwave Ln',
+    //         addressLine2: 'Suite 11',
+    //         locality: 'Edinburgh',
+    //         administrativeDistrictLevel1: 'SCT',
+    //         postalCode: 'EH1 3AA',
+    //         country: 'GB'
+    //     },
+    //     phoneNumber: '+44-131-555-5678',
+    //     note: 'a customer'
+    // },
+    // {
+    //     givenName: 'Leonardo',
+    //     familyName: 'da Vinci',
+    //     emailAddress: 'Leonardo.daVinci@example.com',
+    //     address: {
+    //         addressLine1: '1200 Renaissance Rd',
+    //         addressLine2: 'Apt 12',
+    //         locality: 'Florence',
+    //         administrativeDistrictLevel1: 'TUS',
+    //         postalCode: '50123',
+    //         country: 'IT'
+    //     },
+    //     phoneNumber: '+39-055-555-7890',
+    //     note: 'a customer'
+    // },
+    // {
+    //     givenName: 'Charles',
+    //     familyName: 'Darwin',
+    //     emailAddress: 'Charles.Darwin@example.com',
+    //     address: {
+    //         addressLine1: '1300 Evolution Way',
+    //         addressLine2: 'Suite 13',
+    //         locality: 'London',
+    //         administrativeDistrictLevel1: 'ENG',
+    //         postalCode: 'SW1A 4AA',
+    //         country: 'GB'
+    //     },
+    //     phoneNumber: '+44-20-555-9012',
+    //     note: 'a customer'
+    // },
+    // {
+    //     givenName: 'Louis',
+    //     familyName: 'Pasteur',
+    //     emailAddress: 'Louis.Pasteur@example.com',
+    //     address: {
+    //         addressLine1: '1400 Microbe Ln',
+    //         addressLine2: 'Apt 14',
+    //         locality: 'Paris',
+    //         administrativeDistrictLevel1: 'IDF',
+    //         postalCode: '75006',
+    //         country: 'FR'
+    //     },
+    //     phoneNumber: '+33-1-555-1234',
+    //     note: 'a customer'
+    // },
+    // {
+    //     givenName: 'Sigmund',
+    //     familyName: 'Freud',
+    //     emailAddress: 'Sigmund.Freud@example.com',
+    //     address: {
+    //         addressLine1: '1500 Psychoanalysis Pkwy',
+    //         addressLine2: 'Suite 15',
+    //         locality: 'Vienna',
+    //         administrativeDistrictLevel1: 'W',
+    //         postalCode: '1010',
+    //         country: 'AT'
+    //     },
+    //     phoneNumber: '+43-1-555-5678',
+    //     note: 'a customer'
+    // },
+    {
+        givenName: 'Marie',
+        familyName: 'Antoinette',
+        emailAddress: 'Marie.Antoinette@example.com',
+        address: {
+            addressLine1: '1600 Royal Ct',
+            addressLine2: 'Apt 16',
+            locality: 'Versailles',
+            administrativeDistrictLevel1: 'IDF',
+            postalCode: '78000',
+            country: 'FR'
+        },
+        phoneNumber: '+1-503-234-5678',
+        note: 'a customer'
+    },
+    {
+        givenName: 'William',
+        familyName: 'Shakespeare',
+        emailAddress: 'William.Shakespeare@example.com',
+        address: {
+            addressLine1: '1700 Drama Dr',
+            addressLine2: 'Suite 17',
+            locality: 'Stratford-upon-Avon',
+            administrativeDistrictLevel1: 'ENG',
+            postalCode: 'CV37 6AA',
+            country: 'GB'
+        },
+        phoneNumber: '+1-602-345-6789',
+        note: 'a customer'
+    },
+    // {
+    //     givenName: 'Jane',
+    //     familyName: 'Austen',
+    //     emailAddress: 'Jane.Austen@example.com',
+    //     address: {
+    //         addressLine1: '1800 Novel Rd',
+    //         addressLine2: 'Apt 18',
+    //         locality: 'Winchester',
+    //         administrativeDistrictLevel1: 'ENG',
+    //         postalCode: 'SO23 9AA',
+    //         country: 'GB'
+    //     },
+    //     phoneNumber: '+44-1962-555-7890',
+    //     note: 'a customer'
+    // },
+    // {
+    //     givenName: 'Mozart',
+    //     familyName: 'Amadeus',
+    //     emailAddress: 'Mozart.Amadeus@example.com',
+    //     address: {
+    //         addressLine1: '1900 Symphony St',
+    //         addressLine2: 'Suite 19',
+    //         locality: 'Salzburg',
+    //         administrativeDistrictLevel1: 'S',
+    //         postalCode: '5020',
+    //         country: 'AT'
+    //     },
+    //     phoneNumber: '+43-662-555-1234',
+    //     note: 'a customer'
+    // },
+    // {
+    //     givenName: 'Vincent',
+    //     familyName: 'van Gogh',
+    //     emailAddress: 'Vincent.vanGogh@example.com',
+    //     address: {
+    //         addressLine1: '2000 Starry Night Rd',
+    //         addressLine2: 'Apt 20',
+    //         locality: 'Arles',
+    //         administrativeDistrictLevel1: 'PAC',
+    //         postalCode: '13200',
+    //         country: 'FR'
+    //     },
+    //     phoneNumber: '+33-4-555-5678',
+    //     note: 'a customer'
+    // }
+];
