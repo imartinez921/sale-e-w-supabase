6692fe3089af3057713e66530e64e07c7c9cac53
Irene D. Martinez <imartinez921@gmail.com>
2023-10-05 15:37:41 -0400

    Dashboard page checks if user is logged in

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,35 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
+import type { Database } from "@/lib/database.types";
+import { redirect } from "next/navigation";
 
+// This is our logged-in user's home page
 export default async function Dashboard() {
-    // Wrap dashboard in server-side Supabase client
-    // https://youtu.be/-7K6DRWfEGM?si=6amjfTk8TZFzeKyQ&t=283
-    const supabase = createServerComponentClient({cookies});
+	// Following https://youtu.be/-7K6DRWfEGM?si=6amjfTk8TZFzeKyQ&t=283
+	// Docs have some outdated code:
+	// https://supabase.com/docs/guides/auth/auth-helpers/nextjs?language=ts#server-components
 
-    // I still need to add catalog test data to Supabase to test this connection
-    const {data} = await supabase.from("catalog").select();
-    // return <pre>{JSON.stringify(data,null,2)}</pre>
+	// Give this server component access to cookies
+	const supabase = createServerComponentClient<Database>({ cookies });
+
+	// Check if user is logged in
+	const {
+		data: { session },
+	} = await supabase.auth.getSession();
+	if (!session) {
+		redirect("/unauthenticated");
+	}
+
+	// TODO: Fetch catalog data from Supabase and pass to corresponding component to render
+	// TODO: Fetch customer data from Supabase and pass to corresponding component to render
+	// TODO?: Fetch past email campaigns data from Supabase and pass to corresponding component to render
+	// I still need to add catalog test data to Supabase to test this connection
+	const { data } = await supabase.from("catalog").select();
+	
+    // Testing: Pretty print result
+	// return <pre>{JSON.stringify(data,null,2)}</pre>
+
+	// TODO: Render corresponding components
+	// TODO: Layout.tsx to place components using Tremor UI
 }
