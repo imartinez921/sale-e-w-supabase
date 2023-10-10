0b501df8990d03bc92a796c2674db097a55e1d31
Irene D. Martinez <imartinez921@gmail.com>
2023-10-09 20:20:25 -0400

    Fix BackButton on Unauthenticated page

--- app/unauthenticated/page.tsx
+++ app/unauthenticated/page.tsx
@@ -1,9 +1,9 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-
-import type { Database } from "@/lib/database.types";
 import { redirect } from "next/navigation";
 import Link from "next/link";
+
+import BackButton from "../components/nav/BackButton";
 
 
 export default async function Unauthenticated() {
@@ -19,26 +19,7 @@ export default async function Unauthenticated() {
 	}
 	return (
 		<>
-			<Link
-				href="/"
-				className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					strokeWidth="2"
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-				>
-					<polyline points="15 18 9 12 15 6" />
-				</svg>{" "}
-				Back
-			</Link>
+			<BackButton buttonText="Back" path="/" />
 			<h1 className="text-foreground text-2xl font-bold mb-4">
 				To continue using this app, please sign in.
 			</h1>
