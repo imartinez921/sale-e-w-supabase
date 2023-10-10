31f39e5c87e5cd497066668f0998f191e9482730
Irene D. Martinez <imartinez921@gmail.com>
2023-10-09 20:08:12 -0400

    Add clarifying comments to dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,18 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-// import type { Database } from "@/lib/database.types";
-import Link from "next/link";
+
 import { redirect } from "next/navigation";
 
-// This is our logged-in user's home page
-export default async function Dashboard(
-	{
-		children
-	}: {
-		children: React.ReactNode,
-	}) {
-	// Following https://youtu.be/-7K6DRWfEGM?si=6amjfTk8TZFzeKyQ&t=283
-	// Docs have some outdated code:
-	// https://supabase.com/docs/guides/auth/auth-helpers/nextjs?language=ts#server-components
+import Button from "../components/nav/button";
 
-	// Give this server component access to cookies
+// This is the main overview page after logging in
+export default async function DashboardPage({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	
+	// Give this server component access to user's cookies
 	const supabase = createServerComponentClient({ cookies });
 
 	// Check if user is logged in
@@ -26,8 +23,15 @@ export default async function Dashboard(
 		redirect("/unauthenticated");
 	}
 
+    // Execute all data to be passed to props here
 	// TODO: Fetch catalog data from Supabase and pass to corresponding component to render
 	// TODO: Fetch customer data from Supabase and pass to corresponding component to render
+	{
+		/* <CustomerServerComponent supabase={supabase} /> DO NOT UNCOMMENT THIS OR DATA WILL BE PUT IN SUPABASE WE NO LONGER NWWS */
+	}
+	{
+		/* <CatalogDetail children={undefined} /> COMMENTED FOR SAME REASON AS ABOVE */
+	}
 	// TODO?: Fetch past email campaigns data from Supabase and pass to corresponding component to render
 	// I still need to add catalog test data to Supabase to test this connection
 	const { data } = await supabase.from("catalog").select();
@@ -35,34 +39,14 @@ export default async function Dashboard(
 	// Testing: Pretty print result
 	// return <pre>{JSON.stringify(data,null,2)}</pre>
 
-	// TODO: Render corresponding components
-	// TODO: Layout.tsx to place components using Tremor UI
 	return (
 		<>
-			<Link
-				href="/"
-				className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					strokeWidth="2"
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-				>
-					<polyline points="15 18 9 12 15 6" />
-				</svg>{" "}
-				Back
-			</Link>
 			<h1 className="text-foreground text-2xl font-bold mb-4">
-				You have successfully logged in.
-				<br /> Welcome to your dashboard page.
+				Welcome to your dashboard page.
 			</h1>
+			<main>
+                {/* TODO: Import viewcomponents here: pass results of above as data prop */}
+            </main>
 		</>
 	);
 }
