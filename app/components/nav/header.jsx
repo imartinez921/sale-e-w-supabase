dd5b6b2ac59c6a11a4377fce2fbc2cd05f51f3d8
Irene D. Martinez <imartinez921@gmail.com>
2023-10-10 00:47:21 -0400

    Import svg logo into header

--- app/components/nav/header.jsx
+++ app/components/nav/header.jsx
@@ -1,7 +1,19 @@
+"use client";
+
+import Image from "next/image";
+import logo from "../../assets/images/logo.svg";
+
 export default function Header() {
-  return (
-    <header>
-        <>I'M A HEADER</>
-    </header>
-  );
+  
+	return (
+		<header>
+			<Image
+				priority
+				src={logo}
+				alt="SALE-E logo"
+				width={200}
+				height={50}
+			/>
+		</header>
+	);
 }
