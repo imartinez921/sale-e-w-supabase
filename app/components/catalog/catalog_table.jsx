79a1e316efd9d2f150465c8ebdde304671e5bf09
Irene D. Martinez <imartinez921@gmail.com>
2023-10-10 04:43:26 -0400

    Initiate CatalogTable

--- app/components/catalog/catalog_table.jsx
+++ app/components/catalog/catalog_table.jsx
@@ -1,31 +1,40 @@
-"use client"
+"use client";
+import {
+	Card,
+	Table,
+	TableHead,
+	TableRow,
+	TableHeaderCell,
+	TableBody,
+	TableCell,
+	Text,
+	Title,
+	Badge,
+} from "@tremor/react";
 
-const productCatalog = {
-    1: {itemName: "Green tea", price: 5.50},
-    2: {itemName: "Rose tea", price: 5.50},
-    3: {itemName: "Dark chocolate", price: 6.50},
-    4: {itemName: "Resistance bands", price: 15},
+export default function CatalogTable({ data }) {
+	return (
+		<Card>
+			<Title>Your Catalog</Title>
+			<Table className="mt-5">
+				<TableHead>
+					<TableRow>
+						<TableHeaderCell>ID</TableHeaderCell>
+						<TableHeaderCell>Name</TableHeaderCell>
+						<TableHeaderCell>Description</TableHeaderCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{catalogArray?.map((item) => (
+						<TableRow key={item.id}>
+							<TableCell>{item.name}</TableCell>
+							<TableCell>
+								<Text>{item.description}</Text>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</Card>
+	);
 }
-
-export default function CatalogTable({productCatalog}) {
-    return (
-        <table>
-        <thead>
-            <tr>
-            <th>Id</th>
-            <th>Item</th>
-            <th>Price</th>
-            </tr>
-        </thead>
-        <tbody>
-            {catalog.map((product) => (
-            <tr key={product.id} onClick={() => router.push(`/product/${product.id}`)}>
-                <td>{product.id}</td>
-                <td>{product.itemName}</td>
-                <td>{product.price}</td>
-            </tr>
-            ))}
-        </tbody>
-        </table>
-    )
-    }
