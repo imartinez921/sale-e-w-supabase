0b966b4607f3d293b2a2895d1387ebcdf45bf99f
Irene D. Martinez <imartinez921@gmail.com>
2023-10-10 22:51:39 -0400

    Move Dashboard page structure to layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -40,51 +40,6 @@ export default async function DashboardPage({
 
 	return (
 		<main>
-			<Title>Dashboard</Title>
-			<Text>
-				Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
-			</Text>
-
-			<TabGroup className="mt-6">
-				<TabList>
-					<Tab>Page 1</Tab>
-					<Tab>Page 2</Tab>
-				</TabList>
-				<TabPanels>
-					<TabPanel>
-						<Grid
-							numItemsMd={2}
-							numItemsLg={3}
-							className="gap-6 mt-6"
-						>
-							<Card>
-								{/* Placeholder to set height */}
-								<div className="h-28" />
-							</Card>
-							<Card>
-								{/* Placeholder to set height */}
-								<div className="h-28" />
-							</Card>
-							<Card>
-								{/* Placeholder to set height */}
-								<div className="h-28" />
-							</Card>
-						</Grid>
-						<div className="mt-6">
-							<Card>
-								<div className="h-80" />
-							</Card>
-						</div>
-					</TabPanel>
-					<TabPanel>
-						<div className="mt-6">
-							<Card>
-								<div className="h-96" />
-							</Card>
-						</div>
-					</TabPanel>
-				</TabPanels>
-			</TabGroup>
 		</main>
 	);
 }
