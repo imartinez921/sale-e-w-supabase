6305abb4478165e4e4d9608f9242c4a32198bd71
Noah Medoff <noahmedoff@gmail.com>
2023-10-12 01:49:35 -0700

    working on model tuning etc

--- app/utils/google-vertex-cllient.ts
+++ app/utils/google-vertex-cllient.ts
@@ -1,2 +1,39 @@
-export const projectId = process.env.GOOGLE_APPLICATION_CREDENTIALS
-export const cloudLocation = 'https://us-west1-aiplatform.googleapis.com';
+export const projectId = process.env.CLOUD_PROJECT_ID
+export const cloudLocation = "https://us-central1-aiplatform.googleapis.com"
+import { NextApiRequest, NextApiResponse } from "next";
+
+const { EndpointServiceClient } = require('@google-cloud/aiplatform');
+
+// Specifies the location of the api endpoint
+const clientOptions = {
+    apiEndpoint: 'us-central1-aiplatform.googleapis.com',
+};
+const client = new EndpointServiceClient(clientOptions);
+
+
+export default async function askForCustomerDiscounts(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== 'POST') {
+        return res.status(405).end();
+    }
+
+    const { question } = req.body;
+
+    if (!question) {
+        return res.status(400).json({ error: 'Question is required.' });
+    }
+
+    try {
+        const endpoint = 'YOUR_VERTEX_AI_ENDPOINT'; // Replace with your Vertex AI endpoint
+        const request = {
+            endpoint,
+            instances: [{ content: question }],
+        };
+
+        const [response] = await client.predict(request);
+        const answer = response.predictions[0]?.content;
+
+        return res.json({ answer });
+    } catch (error: any) {
+        return res.status(500).json({ error: error.message });
+    }
+}
