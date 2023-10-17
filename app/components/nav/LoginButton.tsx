acc86c6786e40c95b7e7f8eb9485777f2f967489
Irene D. Martinez <imartinez921@gmail.com>
2023-10-16 20:51:56 -0400

    Add icon and color to LoginButton

--- app/components/nav/LoginButton.tsx
+++ app/components/nav/LoginButton.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { Button } from "@tremor/react";
+import {ArrowRightIcon,
+} from "@heroicons/react/solid";
 
 export default function LoginButton() {
 	return (
 		<form action="/auth/login" method="get">
-			<Button size="xs" variant="secondary">
+			<Button icon={ArrowRightIcon} size="xs" variant="primary">
 				Click here to sign in as demo user
 			</Button>
 		</form>
