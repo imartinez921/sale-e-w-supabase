9fa69ff74b2810953af58f2b4b24208b2fde73a9
Irene D. Martinez <imartinez921@gmail.com>
2023-10-17 19:40:54 -0400

    Rename customer server component to CustomersTab

---
+++ app/components/customers/CustomersTable.jsx
@@ -0,0 +1,67 @@
+"use client";
+import {
+	Card,
+	Table,
+	TableHead,
+	TableRow,
+	TableHeaderCell,
+	TableBody,
+	TableCell,
+	Text,
+	Title,
+	Badge,
+	Button
+} from "@tremor/react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+export default function CustomersTable({ data, palmAI, customerData }) {
+	const supabaseClient = createClientComponentClient()
+
+	return (
+		<>
+			<Title>Your Customers</Title>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableHeaderCell>Name</TableHeaderCell>
+						<TableHeaderCell>Description</TableHeaderCell>
+						<TableHeaderCell>
+							Price (single OR bulk)
+						</TableHeaderCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{data?.map((item) => (
+						<TableRow key={item.id}>
+							<TableCell>{item.name}</TableCell>
+							<TableCell>
+								<Text>{item.description}</Text>
+							</TableCell>
+							<TableCell>
+								<Text>{item.price}</Text>
+							</TableCell>
+							<TableCell>
+								<Button
+									onClick={async () => {
+										const campaign = await palmAI(
+											customerData?.data,
+											item.name
+										);
+
+										const { data, error } =
+											await supabaseClient
+												.from("email_campaigns")
+												.insert([campaign])
+												.select();
+									}}
+								>
+									Create Sale Campaign
+								</Button>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</>
+	);
+}
