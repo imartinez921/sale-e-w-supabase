03b5b0d2797e458d2ab2a31ec1f1f31dc725b429
Irene D. Martinez <imartinez921@gmail.com>
2023-10-18 00:18:32 -0400

    Deploy attempt: Add name to LandingAvatar

--- app/components/nav/LandingAvatar.tsx
+++ app/components/nav/LandingAvatar.tsx
@@ -12,6 +12,7 @@ export default function LandingAvatar({ name, imageUrl, socialUrl }: AvatarProps
     return (
 		<a href={socialUrl}>
 			<Card className="flex flex-col gap-2 justify-center items-center">
+			<Text>{name}</Text>
 				<img src={imageUrl} title={name} alt="" />;
 			</Card>
 		</a>
